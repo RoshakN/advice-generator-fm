b9d18ff27288d09d475d49ebbba2f27f927449fc
RoshakN <msnooraniz@gmail.com>
2023-07-16 21:56:23 +0330

    Implement API

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import Card from "./Card";
+import Axios from "axios";
 
 function App() {
-  const handleClick = () => console.log("clicked");
+  const [sageAdvice, setSageAdvice] = useState({
+    id: 217,
+    advice: "Identify sources of happiness.",
+  });
+  const handleClick = () => {
+    Axios.get("https://api.adviceslip.com/advice").then((res) => {
+      setSageAdvice(res.data.slip);
+      console.log(sageAdvice);
+    });
+  };
+
+  // useEffect(() => {
+  //   Axios.get("https://api.adviceslip.com/advice").then((res) => {
+  //     setSageAdvice(res.data.slip.advice);
+  //     console.log(sageAdvice);
+  //   });
+  // }, []);
 
   return (
     <div className="app-container">
-      <Card handleClick={handleClick} />
+      <Card handleClick={handleClick} sageAdvice={sageAdvice} />
     </div>
   );
 }
--- src/Card.jsx
+++ src/Card.jsx
@@ -3,13 +3,8 @@ import React from "react";
 export default function Card(props) {
   return (
     <div className="container">
-      <p className="id">ADVICE #117</p>
-      <p className="advice">
-        `"Advice Lorem ipsum dolor, sit amet consectetur adipisicing elit. Lorem
-        ipsum dolor sit amet consectetur adipisicing elit. Maiores rem esse,
-        rerum officia itaque neque impedit nam cupiditate commodi corporis magni
-        inventore"`
-      </p>
+      <p className="id">ADVICE #{props.sageAdvice.id}</p>
+      <p className="advice">"{props.sageAdvice.advice}"</p>
       <img
         className="divider"
         src="images/pattern-divider-desktop.svg"
